ddc6c3070c39fdec9cfea9e4ebd0575ff3a6ab8d
Adrian Ko <adrianchko@gmail.com>
2015-03-19 23:27:05 +0000

    Add test data

---
+++ src/main/Scrapify.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Scrapify {
+    
+    public static void main(String[] args) {
+        String path = "/.abc[0]/.def[0]/text()";
+        String path2 = "/.abc[0]/.def[0]/@data-test";
+        
+        String html = "<html><head><title>123</title></head><body><div class=\"abc\"><div class=\"def\" " +
+            "data-test=\"test\">123</div>456</div>789</body></hmtl>";
+    }
+    
+}
