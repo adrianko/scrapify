93f70255b19d0dfe4aa18f8e127778d041c7ea83
Adrian Ko <adrianchko@gmail.com>
2015-03-21 12:48:16 +0000

    add relative path

--- src/test/main/ScrapifyTest.java
+++ src/test/main/ScrapifyTest.java
@@ -11,11 +11,12 @@ import java.util.List;
 public class ScrapifyTest {
     
     Scrapify s = new Scrapify();
-    List<String> path = Arrays.asList("", ".abc[0]", ".def[0]", "text()");
+    List<String> absolutePath = Arrays.asList("", ".abc[0]", ".def[0]", "text()");
+    List<String> relativePath = Arrays.asList(".abc[0]", ".def[0]", "@data-test");
     
     @Test
     public void parse() {
-        List<String> exp = new LinkedList<>(path);
+        List<String> exp = new LinkedList<>(absolutePath);
         List<String> act = s.parse("/.abc[0]/.def[0]/text()");
 
         Assert.assertEquals(exp, act);
@@ -23,9 +24,16 @@ public class ScrapifyTest {
     
     @Test
     public void absoluteTrue() {
-        boolean act = s.absolute(path);
+        boolean act = s.absolute(absolutePath);
         
         Assert.assertTrue(act);
+    }
+    
+    @Test
+    public void absoluteFalse() {
+        boolean act = s.absolute(relativePath);
+        
+        Assert.assertFalse(act);
     }
     
 }
