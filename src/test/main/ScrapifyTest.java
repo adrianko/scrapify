331d3580a84fec7325ccef3ce95c5757f02ef391
Adrian Ko <adrianchko@gmail.com>
2015-03-21 12:55:07 +0000

    use more meaningful name

--- src/test/main/ScrapifyTest.java
+++ src/test/main/ScrapifyTest.java
@@ -10,28 +10,28 @@ import java.util.List;
 
 public class ScrapifyTest {
     
-    Scrapify s = new Scrapify();
+    Scrapify scrapify = new Scrapify();
     List<String> absolutePath = Arrays.asList("", ".abc[0]", ".def[0]", "text()");
     List<String> relativePath = Arrays.asList(".abc[0]", ".def[0]", "@data-test");
     
     @Test
     public void parse() {
         List<String> exp = new LinkedList<>(absolutePath);
-        List<String> act = s.parse("/.abc[0]/.def[0]/text()");
+        List<String> act = scrapify.parse("/.abc[0]/.def[0]/text()");
 
         Assert.assertEquals(exp, act);
     }
     
     @Test
     public void absoluteTrue() {
-        boolean act = s.absolute(absolutePath);
+        boolean act = scrapify.absolute(absolutePath);
         
         Assert.assertTrue(act);
     }
     
     @Test
     public void absoluteFalse() {
-        boolean act = s.absolute(relativePath);
+        boolean act = scrapify.absolute(relativePath);
         
         Assert.assertFalse(act);
     }
