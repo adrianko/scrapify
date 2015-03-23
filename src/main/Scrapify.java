9af919a10c818266b12be23db09659050ff9bc80
Adrian Ko <adrianchko@gmail.com>
2015-03-23 09:57:01 +0000

    read both files

--- src/main/Scrapify.java
+++ src/main/Scrapify.java
@@ -1,11 +1,15 @@
 package main;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Scrapify {
     
@@ -28,6 +32,33 @@ public class Scrapify {
     
     public boolean absolute(List<String> path) {
         return path.get(0).equals("");
+    }
+    
+    public String getHTML(String filePath) {
+        try {
+            return Files.readAllLines(Paths.get(filePath)).toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return "";
+    }
+    
+    public Map<String, String> getPaths(String filePath) {
+        Map<String, String> paths = new HashMap<>();
+
+        try {
+            String file = Files.readAllLines(Paths.get(filePath)).toString();
+            JSONObject json = (JSONObject) new JSONParser().parse(file);
+
+            for (Object key : json.keySet()) {
+                paths.put(key.toString(), String.valueOf(json.get(key.toString())));
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        
+        return paths;
     }
     
 }
