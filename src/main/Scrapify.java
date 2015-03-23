53e9cd2093b48ead7d1395aa1d525054afdf0538
Adrian Ko <adrianchko@gmail.com>
2015-03-23 09:38:17 +0000

    add paths file and src html file

--- src/main/Scrapify.java
+++ src/main/Scrapify.java
@@ -12,15 +12,8 @@ public class Scrapify {
     public String html;
     
     public static void main(String[] args) {
-        String path = "/.abc[0]/.def[0]/text()";
-        String path2 = "/.abc[0]/.def[0]/@data-test";
-        
-        String html = "<html><head><title>123</title></head><body><div class=\"abc\"><div class=\"def\" " +
-            "data-test=\"test\">123</div>456</div>789</body></html>";
-        
         Scrapify s = new Scrapify();
-        s.setHTML(html);
-        s.parse(path);
+        
     }
     
     public void setHTML(String html) {
