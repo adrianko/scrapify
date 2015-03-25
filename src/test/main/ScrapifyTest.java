7f5f0b4f6a0fc2e8b7a57401b3fec98d24284497
Adrian Ko <adrianchko@gmail.com>
2015-03-25 16:00:18 +0000

    Add test

--- src/test/main/ScrapifyTest.java
+++ src/test/main/ScrapifyTest.java
@@ -4,11 +4,16 @@ import main.Scrapify;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ScrapifyTest {
-    
+
+    public static String basePath = new File(ScrapifyTest.class.getResource(".").getPath() + "../../../../../")
+        .getAbsolutePath();
     Scrapify scrapify = new Scrapify();
     List<String> absolutePath = Arrays.asList("", ".abc[0]", ".def[0]", "text()");
     List<String> relativePath = Arrays.asList(".abc[0]", ".def[0]", "@data-test");
@@ -31,6 +36,17 @@ public class ScrapifyTest {
         boolean act = scrapify.absolute(relativePath);
         
         Assert.assertFalse(act);
+    }
+    
+    @Test
+    public void getPaths() {
+        Map<String, String> exp = new HashMap<>();
+        exp.put("text", String.join("/", absolutePath));
+        exp.put("attr", "/" + String.join("/", relativePath));
+
+        Map<String, String> act = scrapify.getPaths(basePath + "/paths.json");
+        
+        Assert.assertEquals(exp, act);
     }
     
 }
